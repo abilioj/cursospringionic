39166c9e9b7890667b5ff073f9d1329c5aa6d366
abilioj <ajgfqbu@gmail.com>
2021-01-15 17:29:02 -0300

    update - C

---
+++ src/pages/cart/cart.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CartItem } from '../../models/cart-item';
+import { CartService } from '../../services/domain/cart.service';
+import { ProdutoService } from '../../services/domain/produto.service';
+import { StorageService } from '../../services/storage.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-cart',
+  templateUrl: 'cart.html',
+})
+export class CartPage {
+
+  items: CartItem[];
+
+  constructor(
+    public navCtrl: NavController
+    , public navParams: NavParams
+    , public cartService: CartService
+    , public produtosService: ProdutoService
+    ,public storage: StorageService
+  ) {
+  }
+
+  ionViewDidLoad() {
+    // let cart = this.storage.getCart();
+    let cart = this.cartService.getCart();
+    this.items = cart.items;
+    this.loadImageUrls();
+  }
+
+  loadImageUrls() {
+    for (var i = 0; i <= this.items.length; i++) {
+      try {
+        let item = this.items[i];
+        this.produtosService.getSmallImageFromBucket(item.produto.id)
+          .subscribe(response => {
+            item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.produto.id}-small.jpg`;
+          }, error => { });
+      } catch (e) {
+
+      }
+    }
+  }
+}
--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from './../../config/api.config';
 
@@ -17,6 +18,7 @@ export class ProdutoDetailPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public produtoService: ProdutoService
+    ,public cartService: CartService
   ) {}
 
   ionViewDidLoad() {
@@ -36,6 +38,11 @@ export class ProdutoDetailPage {
       },
         error => { }
       );
+  }
+
+  addToCart(produto: ProdutoDTO){
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
