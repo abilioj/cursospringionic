920c95450a411c345242b447f378344778c455e8
abilioj <ajgfqbu@gmail.com>
2021-01-15 00:04:19 -0300

    update de Correcao

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -13,8 +13,8 @@ import { ClienteService } from '../../services/domain/cliente.service';
 
 export class ProfilePage {
 
-    cliente: ClienteDTO;
-    imageUrlDefull: string;
+  cliente: ClienteDTO;
+  imageUrlDefull: string;
 
   constructor(
     public navCtrl: NavController
@@ -28,16 +28,16 @@ export class ProfilePage {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.cliente = response;
-        this.getImageIfExists();
-      },
-      error => {
-        if (error.status == 403) {
-          this.navCtrl.setRoot('HomePage');
-        }
-      });
-    }else{
+        .subscribe(response => {
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    } else {
       this.navCtrl.setRoot('HomePage');
     }
   }
@@ -45,9 +45,9 @@ export class ProfilePage {
   //pega a imgem la no S3 Amazon
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${parseInt(this.cliente.id)}.jpg`;
-    },
-    error => {});
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${parseInt(this.cliente.id)}.jpg`;
+      },
+        error => { });
   }
 }
