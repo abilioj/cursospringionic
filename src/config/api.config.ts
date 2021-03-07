aa56a686b068588b8d1b3eaa4576f880750cc403
abilioj <ajgfqbu@gmail.com>
2021-03-07 17:49:19 -0300

    correção

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
     //    baseUrl: "https://spring-ionic-nelio.herokuapp.com",
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8081"
     , bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/cursos-java-spring-boot-no-backend-ionic-no-front-end"
 }
--- src/models/cliente.dto.ts
+++ src/models/cliente.dto.ts
@@ -1,13 +1,6 @@
 export interface ClienteDTO {
     id: string;
-<<<<<<< HEAD
-   'nome': string;
-    'email': string;
-    'imageUrl'?: string;
-}
-=======
     nome: string;
-    email: string; 
+    email: string;
     imageUrl?: string;
-} 
->>>>>>> e42884f134e84a3eb3b60c34be4ba0d52a094314
+}
